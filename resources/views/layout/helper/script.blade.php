63ce6750072967d8ac5ad7b47f80eff788843c25
juanframart2011 <juanframart2011@gmail.com>
2022-05-10 14:41:49 -0500

    depurado js

--- resources/views/layout/helper/script.blade.php
+++ resources/views/layout/helper/script.blade.php
@@ -5,6 +5,5 @@
 <!-- Aos Scripts -->
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script src="dist/js/main.js"></script>
-<script src="dist/js/home.js"></script>
 
 @yield( "scriptExtra" )
